7b12eeba350a7e522f0a677a539840f6e5b7cd88
Simon-mircevski <63059995+Simon-mircevski@users.noreply.github.com>
2022-08-30 12:06:47 +0200

    Add files via upload

---
+++ VPFinkiBirdProekt/VPFinkiBirdProekt/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VPFinkiBirdProekt
+{
+    public partial class Form1 : Form
+    {
+        int score = 0;
+        int pipeSpeed = 8;
+        int gravity = 10;
+        Random rnd = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void gameTimerEvent(object sender, EventArgs e)
+        {
+            FinkiBird.Top += gravity;
+            pipeBottom.Left -= pipeSpeed;
+            pipeTop.Left -= pipeSpeed;
+            ScoreLabel.Text = "Score: " + score;     
+
+            if(pipeTop.Left < -100)
+            {
+                pipeTop.Left = rnd.Next(65, 100) * 10;
+                score++;
+            }
+            if(pipeBottom.Left < -100)
+            {
+                pipeBottom.Left = rnd.Next(65, 100) * 10;
+                score++;
+            }
+
+            if(FinkiBird.Bounds.IntersectsWith(pipeBottom.Bounds)||
+                FinkiBird.Bounds.IntersectsWith(pipeTop.Bounds) ||
+                FinkiBird.Bounds.IntersectsWith(grounds.Bounds) ||
+                FinkiBird.Top < -25)
+            {
+                endGame();
+            }
+            if (score > 5)
+            {
+                pipeSpeed = 15;
+            }
+            if(score > 15)
+            {
+                pipeSpeed = 20;
+            }
+            if (score > 50)
+            {
+                pipeSpeed = 30;
+            }
+           
+        }
+
+        private void gameKeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Space)
+            {
+                gravity = -10;
+            }
+        }
+
+        private void gameKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = 10;
+            }
+        }
+
+        private void endGame()
+        {
+            timer1.Stop();
+            ScoreLabel.Text = "Game over!!! Your score was : " + score ;
+        }
+    }
+}
